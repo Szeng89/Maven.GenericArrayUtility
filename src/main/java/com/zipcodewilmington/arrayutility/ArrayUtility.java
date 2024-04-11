b4a9a13f85e6892a03bad7d4c7c8c6890774d960
Szeng89 <shijinz898@gmail.com>
2024-04-10 21:50:24 -0400

    started, commented out all tests but one to make fields and constructor, made class generic and will be using arraylist

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,22 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    ArrayList<T> arrayList;
+    T[] array;
+
+    public ArrayUtility(T[] inputArray) {
+        this.array=inputArray;
+        this.arrayList = new ArrayList<>(Arrays.asList(inputArray));
+    }
+
+
+    public int getNumberOfOccurrences(T valueToEvaluate) {
+        return 0;
+    }
 }
