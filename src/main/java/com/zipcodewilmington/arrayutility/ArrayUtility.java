bc570903697672165b8b208df106115c82fa3547
Szeng89 <shijinz898@gmail.com>
2024-04-10 22:03:34 -0400

    made class generic, started with getNumOfOccurence method because it can be called on by other unwritten methods

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -6,17 +6,26 @@ import java.util.Arrays;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility<T> {
+public class ArrayUtility<T> { //made class generic
     ArrayList<T> arrayList;
     T[] array;
 
     public ArrayUtility(T[] inputArray) {
         this.array=inputArray;
-        this.arrayList = new ArrayList<>(Arrays.asList(inputArray));
+        this.arrayList = new ArrayList<>(Arrays.asList(inputArray)); // making array into arraylist because its mutable
     }
 
 
     public int getNumberOfOccurrences(T valueToEvaluate) {
-        return 0;
+        int occurrenceCount = 0; // initiaize Occurrence counter to 0;
+
+        for (int i = 0; i < this.arrayList.size(); i ++) { // loop through arrayList
+            T value = this.arrayList.get(i); // assign i to T value
+            if (value.equals(valueToEvaluate)) { // if value is equal to parameter
+                occurrenceCount++; // increment count
+            }
+
+        }
+        return occurrenceCount;
     }
 }
